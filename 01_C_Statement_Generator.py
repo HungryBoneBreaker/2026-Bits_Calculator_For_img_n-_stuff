00b2e414d041ea1fe2960c6d0a870613e38c2adb
HungryBoneBreaker <medillaj50277@masseyhigh.school.nz>
2026-08-20 14:40:41 +1200

    Bits_Calc(Final).py to 01_C_Statement_Generator.py
    Updated

--- 01_C_Statement_Generator.py
+++ 01_C_Statement_Generator.py
@@ -1,4 +1,4 @@
 def statement_generator(statement, decoration):
     print(f"\n{decoration * 5} {statement} {decoration * 5}")
 
-statement_generator (statement: print("Instruction"), decorations print("-"))
+statement_generator("Instruction", "-")
